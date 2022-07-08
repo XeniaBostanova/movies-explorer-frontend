e47044821fb20d9b097e20d21edc951071584c8f
Xenia Bostanova <ksenia.bostanova@gmail.com>
2022-07-08 16:18:45 +0600

    feat: сверстать компоненты AboutProject, Techs

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import './AboutProject.css';
 
 function AboutProject() {
-  return(
-    <p> О проекте</p>
+  return (
+    <section className="project" id="aboutProject">
+      <h2 className="project__title">О проекте</h2>
+      <div className="project__main-container">
+        <div className="project__container">
+          <h3 className="project__subtitle">Дипломный проект включал 5 этапов</h3>
+          <p className="project__description">Составление плана, работу над бэкендом, 
+          вёрстку, добавление функциональности и финальные доработки.</p>
+        </div>
+        <div className="project__container">
+          <h3 className="project__subtitle">На выполнение диплома ушло 5 недель</h3>
+          <p className="project__description">У каждого этапа был мягкий и жёсткий дедлайн, 
+          которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </div>
+      </div>
+      
+      <div className="project__duration-container">
+        <p className="project__duration project__duration_type_backend">1 неделя</p>
+        <p className="project__duration project__duration_type_frontend">4 недели</p>
+      </div>
+      <div className="project__task-container">
+        <p className="project__task project__task_type_backend">Backend</p>
+        <p className="project__task project__task_type_frontend">Frontend</p>
+      </div>
+    </section>
   )
 }
 
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -8,9 +8,9 @@ function Promo() {
       <div className="promo__container">
         <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
         <p className="promo__subtitle">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <button type="button" className="promo__button">Узнать больше</button>
+        <a href="#aboutProject"><button type="button" className="promo__button">Узнать больше</button></a>
       </div>
-      <a href="#aboutProject"><img src={landingLogo} alt="Лого учебного проекта" className="promo__logo" /></a>
+      <img src={landingLogo} alt="Лого учебного проекта" className="promo__logo" />
     </section>
   )
 }
